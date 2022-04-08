5719402bbf3e7a3f6c8f2d874c808f17e18ca67a
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2022-04-08 22:15:06 +0530

    code

---
+++ src/Components/ResturantCard.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+const ResturantCard = (props) => {
+    console.log(props);
+    const newPaymentArray = Object.entries(props.payment_methods).map(item => item[1] ? item[0] : "")
+    return (
+        <>
+        <div style={{
+            border : "2px solid black",
+            margin : "10px"}}>
+            <div style={{display:"flex", justifyContent:"center", alignItem:"center"}}>
+                <div style={{flex : 2}}>
+                    <img src={props.image} alt={props.username} width="200px" height="200px" />
+                </div>
+                <div>
+                    <h3>{props.username}</h3>
+                    <p>{props.address.join(",")}</p>
+                    <p>Cost : {props.costforone}</p>
+                    <p>Accepts : {newPaymentArray.join(",")}</p>
+                </div>
+                <div>
+                    <div style={{backgroundColor : "green", padding:"10px", borderRadius:"50px" }}>{props.rating}</div>
+                    <div>{props.votes}</div>
+                    <div>{props.reviews}</div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export { ResturantCard }
---
+++ src/Components/ResturantDetails.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import "./ResturantDetails.css"
+import data from "../db.json"
+import { ResturantCard } from "./ResturantCard";
+
+
+const ResturantDetails = () => {
+    const [resdata, setResData] = useState(data);
+    const [fliterRating, setFilterRating] = useState(0);
+    const [paymentMethod, setPaymentMethod] = useState("all");
+
+    const handleRating = (rating) => {
+        //console.log("rating",rating);
+        setFilterRating(rating);
+    };
+
+    const handlePayment = (payment_methods) => {
+        //console.log(paymentType,paymentType);
+        setPaymentMethod(payment_methods);
+    }
+
+    const filterLogic = ({rating, payment_methods}) => {
+        //console.log("data",data);
+
+        const { card, cash, upi } = payment_methods;
+        
+        let paymentMode = true;
+        if(paymentMethod === "cash"){
+            paymentMode= cash
+        }else if(paymentMethod === "card"){
+            paymentMode =  card;
+        }else if(paymentMethod === "upi"){
+            paymentMode = upi;
+        }
+
+        return rating >= fliterRating && paymentMode;
+    }
+    return (
+    <>
+    <h1>Resturant Details</h1>;
+    {
+        [4,3,2,1,0].map((rating) => {
+            return <button key={rating} onClick={() => handleRating(rating)}>{rating? `${rating} and above` : "All"}</button>
+        })
+    };
+    {["card","cash","upi","all"].map((payment_methods) => {
+        return (
+            <button key={payment_methods} onClick={() => handlePayment(payment_methods)}>{payment_methods}</button>
+        )
+    })}
+    <div className="restaurantcards">
+    {resdata.filter(filterLogic)
+    .map((item,index) => {
+        return <ResturantCard key={item.id} {...item} />
+    })}
+    </div>
+    </>
+    )
+
+}
+
+export { ResturantDetails }
